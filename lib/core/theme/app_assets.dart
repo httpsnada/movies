8f252fc47575592986cd7c12fe28470c8fd5157e
nada <nadamaklad13113@gmail.com>
2025-11-07 22:03:54 +0200

    feat: Add core theme, assets, and routing
    Introduces the foundational elements for the application's theme, assets, and navigation.
    
    - Adds `AppTheme` with a predefined dark theme, including color schemes, text styles, and widget themes.
    - Creates `AppAssets` to centralize and manage all image and icon asset paths.
    - Establishes an `AppRoutes` enum to define application route names.
    - Updates `main.dart` to apply the new dark theme and set up the initial routing structure.
    - Removes the `fluttertoast` dependency from `pubspec.yaml`.

---
+++ lib/core/theme/app_assets.dart
@@ -0,0 +1,49 @@
+class AppImages {
+  static const String onboarding1 = 'assets/images/onboarding1.png';
+  static const String onboarding2 = 'assets/images/onboarding2.png';
+  static const String onboarding3 = 'assets/images/onboarding3.png';
+  static const String onboarding4 = 'assets/images/onboarding4.png';
+  static const String onboarding5 = 'assets/images/onboarding5.png';
+  static const String onboarding6 = 'assets/images/onboarding6.png';
+
+  static const String avatar1 = 'assets/images/avatar1.png';
+  static const String avatar2 = 'assets/images/avatar2.png';
+  static const String avatar3 = 'assets/images/avatar3.png';
+  static const String avatar4 = 'assets/images/avatar4.png';
+  static const String avatar5 = 'assets/images/avatar5.png';
+  static const String avatar6 = 'assets/images/avatar6.png';
+  static const String avatar7 = 'assets/images/avatar7.png';
+  static const String avatar8 = 'assets/images/avatar8.png';
+  static const String avatar9 = 'assets/images/avatar9.png';
+
+  static const String splash = 'assets/images/splash.png';
+  static const String logo = 'assets/images/logo.png';
+  static const String branding = 'assets/images/branding.png';
+  static const String available = 'assets/images/available_now.png';
+  static const String placeholder = 'assets/images/placeholder.png';
+  static const String popcorn = 'assets/images/popcorn.png';
+  static const String watch_now = 'assets/images/watch_now.png';
+  static const String forgot_password = 'assets/images/forgot_password.png';
+}
+
+class AppIcons {
+  static const String home = 'assets/icons/home.svg';
+  static const String homeSelected = 'assets/icons/home_selected.svg';
+  static const String browse = "assets/icons/browse.svg";
+  static const String browseSelected = 'assets/icons/browse_selected.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String searchSelected = 'assets/icons/search_selected.svg';
+  static const String profile = 'assets/icons/profile.svg';
+  static const String profileSelected = 'assets/icons/profile_selected.svg';
+  static const String duration = 'assets/icons/duration.svg';
+  static const String email = 'assets/icons/email.svg';
+  static const String favorite = 'assets/icons/favorite.svg';
+  static const String history = 'assets/icons/history.svg';
+  static const String name = 'assets/icons/name.svg';
+  static const String password = 'assets/icons/password.svg';
+  static const String phone = 'assets/icons/phone.svg';
+  static const String rate = 'assets/icons/rate.svg';
+  static const String update_profile_name =
+      'assets/icons/update_profile_name.svg';
+  static const String watch_list = 'assets/icons/watch_list.svg';
+}
---
+++ lib/core/theme/app_theme.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class AppColors {
+  static const Color primary = Color(0XFF121312);
+  static const Color white = Color(0XFFFFFFFF);
+  static const Color charcoal = Color(0XFF282A28);
+  static const Color yellow = Color(0XFFF6BD00);
+  static const Color red = Color(0XFFE82626);
+
+  // gradient colors
+}
+
+class AppTheme {
+  static ThemeData darkTheme = ThemeData(
+    colorScheme: ColorScheme.light(primary: AppColors.primary),
+    scaffoldBackgroundColor: AppColors.primary,
+
+    appBarTheme: AppBarTheme(
+      backgroundColor: AppColors.primary,
+      centerTitle: true,
+      foregroundColor: AppColors.yellow,
+      titleTextStyle: TextStyle(
+        color: AppColors.yellow,
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        fontFamily: GoogleFonts.inter().fontFamily,
+      ),
+    ),
+
+    bottomNavigationBarTheme: BottomNavigationBarThemeData(
+      backgroundColor: AppColors.charcoal,
+      selectedItemColor: AppColors.yellow,
+      unselectedItemColor: AppColors.white,
+      type: BottomNavigationBarType.fixed,
+      showSelectedLabels: false,
+      showUnselectedLabels: false,
+      elevation: 3,
+    ),
+
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: AppColors.charcoal,
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+      hintStyle: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        color: AppColors.white,
+        fontFamily: GoogleFonts.inter().fontFamily,
+      ),
+      contentPadding: EdgeInsets.all(12),
+      prefixIconColor: AppColors.white,
+      suffixIconColor: AppColors.white,
+
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16),
+        borderSide: BorderSide(color: AppColors.yellow, width: 1.5),
+      ),
+
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16),
+        borderSide: BorderSide(color: AppColors.red, width: 1.5),
+      ),
+
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(16),
+        borderSide: BorderSide(color: AppColors.red, width: 1.5),
+      ),
+    ),
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.yellow,
+        foregroundColor: AppColors.primary,
+        padding: EdgeInsets.symmetric(vertical: 16),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+        textStyle: TextStyle(
+          color: AppColors.primary,
+          fontSize: 20,
+          fontWeight: FontWeight.w400,
+          fontFamily: GoogleFonts.roboto().fontFamily,
+        ),
+      ),
+    ),
+
+    textTheme: TextTheme(
+      titleLarge: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.w700,
+        color: AppColors.white,
+        fontFamily: GoogleFonts.roboto().fontFamily,
+      ),
+      titleMedium: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w400,
+        color: AppColors.white,
+        fontFamily: GoogleFonts.roboto().fontFamily,
+      ),
+      titleSmall: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w700,
+        color: AppColors.white,
+        fontFamily: GoogleFonts.roboto().fontFamily,
+      ),
+      bodyLarge: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w700,
+        color: AppColors.yellow,
+        fontFamily: GoogleFonts.inter().fontFamily,
+      ),
+      bodyMedium: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        color: AppColors.white,
+        fontFamily: GoogleFonts.roboto().fontFamily,
+      ),
+      bodySmall: TextStyle(
+        fontSize: 14,
+        fontWeight: FontWeight.w400,
+        color: AppColors.white,
+        fontFamily: GoogleFonts.roboto().fontFamily,
+      ),
+    ),
+  );
+}
